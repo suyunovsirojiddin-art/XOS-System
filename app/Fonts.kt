8e661338c8b0566d8154fc099f3380bd6373f166
suyunovsirojiddin-art <suyunovsirojiddin@gmail.com>
2026-08-28 09:42:56 +0500

    Add Fonts.kt for font family definitions

---
+++ app/Fonts.kt
@@ -0,0 +1,11 @@
+package com.xos.personalsystem.presentation.theme
+
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.platform.Font
+
+val XOSFonts = object {
+    val monospace = FontFamily.Monospace
+    val sansSerif = FontFamily.SansSerif
+    val serif = FontFamily.Serif
+}

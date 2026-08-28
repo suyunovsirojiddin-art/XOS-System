702dc92cc895ccd91a12821c7cb6013aaab7c2f5
suyunovsirojiddin-art <suyunovsirojiddin@gmail.com>
2026-08-28 20:16:01 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,6 @@ dependencies {
     
     // Room Database
     implementation("androidx.room:room-runtime:2.6.0")
-    implementation("androidx.room:room-ktx:2.6.0")
     ksp("androidx.room:room-compiler:2.6.0")
     
     // Hilt

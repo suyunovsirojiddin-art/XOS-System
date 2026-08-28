f48159b3f98516b842ac819b684fdd44e32116cb
suyunovsirojiddin-art <suyunovsirojiddin@gmail.com>
2026-08-28 19:29:53 +0500

    Update Kotlin and KSP plugin versions to 1.9.22

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,31 @@
 // build.gradle.kts (Project level)
+
 plugins {
     id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
-    id("org.jetbrains.kotlin.kapt") version "1.9.20" apply false
-    id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
+    id("org.jetbrains.kotlin.kapt") version "1.9.22" apply false
+    id("com.google.devtools.ksp") version "1.9.22-1.0.16" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
